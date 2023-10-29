b2bcbf5a31447855ffb168f2fa6db682b0af74e2
evan8383 <evanbennett83@gmai.com>
2023-10-29 19:57:53 -0400

    Finished API calls to fill site data. Added buttons and funtionality to switch between upcoming games. The current viewable game is saved to local storage.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,27 +1,96 @@
-// working functionality for adding main event title.
-
-const url = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/401547508?lang=en&region=us'
-let eventName
 const awayTeamSpan = document.querySelector('.event-name span:first-child')
 const homeTeamSpan = document.querySelector('.event-name span:last-child')
 const teamSection = document.querySelector('#teamSection')
 const newsSection = document.querySelector('#newsSection')
 
-// fetch(url)
-// .then((res) => {
-//   return res.json()
-// })
-// .then((data) =>{
-//   eventName = data.name
-//   console.log(eventName)
-//   let arr =[]
-//   arr = eventName.split(' at ')
-//   console.log(arr)
-//   awayTeamSpan.textContent = arr[0]
-//   homeTeamSpan.textContent = arr[1]
-// })
+const nextEventDateAndTime = document.querySelector('#nextEventDateAndTime')
+const nextStadium = document.querySelector('#nextStadium')
+const nextForecast = document.querySelector('#nextForecast')
+const gameLink = document.querySelector('#gameLink')
+
+const homeName = document.querySelector('#homeName')
+const homeLogo = document.querySelector('#homeLogo')
+
+const awayName = document.querySelector('#awayName')
+const awayLogo = document.querySelector('#awayLogo')
+
+
+const eventUrl = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/'
 const teamUrl = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams"
-async function getTeams() {
+const articleUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/news?limit=8'
+
+let eventIndex = 0
+let gamesPerWeek
+// async function selectNextEvent(nextEventUrl) {
+//   const response = await fetch(nextEventUrl);
+//   const data = await response.json();
+
+//   const selectedEvent = data.items[eventIndex].$ref
+//   getNextEventDetails(selectedEvent)
+// }
+// selects from a list of event from the current week
+// !Function V1, gets random single game.
+async function selectNextEvent(nextEventUrl) {
+  const response = await fetch(nextEventUrl);
+  const data = await response.json();
+
+  gamesPerWeek = data.items.length
+  getGameIndex()
+  const selectedEvent = data.items[eventIndex].$ref
+  getNextEventDetails(selectedEvent)
+}
+
+// !gets the details of the selected upcoming game.
+async function getNextEventDetails(eventUrl) {
+  const response = await fetch(eventUrl);
+  const data = await response.json()
+
+  const dataAndTime = data.date
+  nextEventDateAndTime.textContent = convertTimeAndDate(dataAndTime)
+  nextStadium.textContent = data.competitions[0].venue.fullName
+  try {
+    nextForecast.textContent = `${data.weather.displayValue}, ${data.weather.temperature} F`
+  } catch {
+    nextForecast.textContent = 'Weather not available'
+  }
+  console.log(data)
+  gameLink.textContent = 'Click for more details'
+  gameLink.setAttribute('href', data.links[0].href)
+
+  const homeTeamUrl = data.competitions[0].competitors[0].team.$ref
+  const awayTeamUrl = data.competitions[0].competitors[1].team.$ref
+
+  getHomeTeam(homeTeamUrl)
+  getAwayTeam(awayTeamUrl)
+}
+async function getHomeTeam(homeTeamUrl) {
+  const response = await fetch(homeTeamUrl)
+  const data = await response.json();
+  homeLogo.setAttribute('src', data.logos[0].href)
+  homeLogo.setAttribute('alt', 'Team Logo')
+
+  homeName.textContent = data.shortDisplayName
+}
+
+async function getAwayTeam(awayTeamUrl) {
+  const response = await fetch(awayTeamUrl)
+  const data = await response.json();
+  awayLogo.setAttribute('src', data.logos[0].href)
+  awayLogo.setAttribute('alt', 'Team Logo')
+
+  awayName.textContent = data.shortDisplayName
+}
+
+// !converts date returned from API to more readable format.
+function convertTimeAndDate(timeAndDate) {
+  const inputTime = timeAndDate;
+  const parsedDate = new Date(inputTime);
+
+  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
+  return parsedDate.toLocaleString('en-US', options);
+}
+
+async function getTeams(teamUrl) {
   const response = await fetch(teamUrl);
   const data = await response.json();
   console.log(data)
@@ -36,13 +105,11 @@ async function getTeams() {
     teamSection.appendChild(teamDiv)
   })
 }
-getTeams()
 
-const articleUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/news?limit=8'
-async function getNews(){
+async function getNews(articleUrl) {
   const response = await fetch(articleUrl);
   const data = await response.json();
-  data.articles.forEach(article =>{
+  data.articles.forEach(article => {
     const newsDiv = document.createElement('div')
     const articleHeader = document.createElement('h3')
     const articleDescription = document.createElement('p')
@@ -54,6 +121,7 @@ async function getNews(){
     articleAuthor.textContent = article.byline
     fullArticleLink.textContent = 'Read the full article'
     fullArticleLink.setAttribute('href', article.links.web.href)
+    fullArticleLink.setAttribute('target', '_blank')
 
     newsDiv.appendChild(articleHeader)
     newsDiv.appendChild(articleDescription)
@@ -62,4 +130,54 @@ async function getNews(){
     newsSection.appendChild(newsDiv)
   })
 }
-getNews()
+function saveGameIndex() {
+  localStorage.setItem('gameIndex', eventIndex)
+}
+function getGameIndex() {
+  const gameIndex = localStorage.getItem('gameIndex')
+  if (localStorage.getItem('gameIndex') === null){
+    eventIndex = 0
+  } else {
+    eventIndex = gameIndex
+  }
+}
+const prevGameBtn = document.querySelector('#prevGameBtn')
+const nextGameBtn = document.querySelector('#nextGameBtn')
+prevGameBtn.addEventListener('click', () => {
+  fetch(eventUrl)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      let numberOfGames = data.items.length
+      eventIndex--
+      console.log(eventIndex)
+      if (eventIndex < 0) {
+        eventIndex = numberOfGames - 1
+        console.log(eventIndex)
+      }
+      saveGameIndex()
+      selectNextEvent(eventUrl)
+    })
+})
+nextGameBtn.addEventListener('click', () => {
+  fetch(eventUrl)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      let numberOfGames = data.items.length
+      eventIndex++
+      console.log(eventIndex)
+      if (eventIndex > numberOfGames - 1) {
+        eventIndex = 0
+        console.log(eventIndex)
+      }
+      saveGameIndex()
+      selectNextEvent(eventUrl)
+    })
+})
+
+selectNextEvent(eventUrl)
+getTeams(teamUrl)
+getNews(articleUrl)
