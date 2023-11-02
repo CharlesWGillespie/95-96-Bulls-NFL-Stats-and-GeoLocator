67e40378b481d82f4ac3ee5991a7fb28181d5015
Robert Totten <rtotten46@gmail.com>
2023-11-02 18:15:29 -0400

    Javascript updates

--- assets/js/stadiums.js
+++ assets/js/stadiums.js
@@ -23,6 +23,89 @@ function fetchStadiumImage(url) {
 }
 fetchStadiumImage (stadiumurl)
 
+// const teamList = document.getElementsById("team-list");
+// const searchBtn = document.getElementsById("searchBtn");
+// const userInput = document.getElementsById("userInput");
+
+
+
+
+// fetch(teamUrl)
+//    .then(response => response.json())
+//    .then(data => {
+//       const teams = data.sports[0].leagues[0].teams;
+//       const teamNames = [];
+//       teams.forEach(team => {
+//          const teamName = team.team.displayName;
+//          teamNames.push(teamName);
+//       });
+
+//       displayTeams(teamNames);
+//       userInput.addEventListener("input", function () {
+//          const searchTerm = searchInput.ariaValueMax.toLowerCase();
+//          const filteredTeams = teamNames.filter(teamName => teamName.toLowerCase().includes(searchTerm));
+//          displayTeams(filteredTeams);
+//       });
+//    })
+
+   // function displayTeams(teams) {
+   //    teamList.innerHTML = "";
+   //    teams.forEach(teamName => {
+   //       const listItem = document.createElement("li");
+   //       listItem.textContent = teamName;
+   //       teamList.appendChild(listItem);
+   //    });
+   // }
+const teamUrl = document.getElementsById("https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/12")
+
+function getVenueImageByInput(userInput) {
+fetch(teamUrl)
+   .then((response) => {
+      if(response.status === 200) {
+         return response.json();
+      } else {
+         throw new Error("Failed to fetch data from the first API");
+      }
+   })
+   .then((data) => {
+      const teamName = data.team.displayName;
+      const teamId = data.team.id;
+      if (userInput.toLowerCase() === teamName.toLowerCase()) {
+         console.log(`User input matches the team name: ${teamName}`);
+         console.log(`Team ID: ${teamId}`);
+         const secondApiUrl = `https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2023/teams/${teamId}`;
+         fetch(secondApiUrl)
+         .then((response) => {
+           if (response.status === 200) {
+             return response.json();
+           } else {
+             throw new Error("Failed to fetch data from the second API");
+           }
+         })
+         .then((data) => {
+           const venueImage = data.team.venue.image;
+           console.log(`Venue Image URL: ${venueImage}`)
+         })
+         .catch((error) => {
+           console.error(error);
+         });
+     } else {
+       console.log(`User input does not match the team name: ${teamName}`);
+     }
+   })
+   .catch((error) => {
+     console.error(error);
+   });
+}
+const userInput = "Green Bay Packers";
+getVenueImageByInput(userInput);
+
+
+
+
+
+
+
 /* store users input to a variable
 fetch to https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams
 add if statement to match useres input to team name
